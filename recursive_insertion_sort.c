f09399186caea9321817d09032adcd04d0a35142
Hosam <hossamnasser938@gmail.com>
2018-06-21 21:18:09 +0200

    Remove unnecessary input to recursive insertion sort algorithm

--- recursive_insertion_sort.c
+++ recursive_insertion_sort.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #define INPUT_SIZE 100000
 
-void recursive_insertion_sort(int[], int, int);
+void recursive_insertion_sort(int[], int);
 
 int main(void){
 	int arr[INPUT_SIZE];
@@ -12,7 +12,7 @@ int main(void){
 		arr[i] = random();
 	}
 	clock_t start_time = clock();
-        recursive_insertion_sort(arr, 0, INPUT_SIZE - 1);
+        recursive_insertion_sort(arr, INPUT_SIZE - 1);
 	clock_t end_time = clock();
 	double execution_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;        
 	/*for(i = 0; i < INPUT_SIZE; i++){
@@ -24,12 +24,11 @@ int main(void){
 
 /*
 ** A is the array to be sorted
-** s is the index that points to the last item in the sorted part
 ** u is the index that points to tha item to be inserted in the sorted part
 */
-void recursive_insertion_sort(int A[], int s, int u){
-	if(u - s > 1)
-		recursive_insertion_sort(A, s, u - 1);
+void recursive_insertion_sort(int A[], int u){
+	if(u > 1)
+		recursive_insertion_sort(A, u - 1);
 	int i = u - 1, key = A[u];
 	while(i >= 0 && A[i] > key){
 		A[i + 1] = A[i];
